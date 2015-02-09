b85eb2c696e9852d84dea360fbb0512bd7b46cb2
GrandmasterShadowMorgue <kavijay1997@yahoo.in>
2015-02-09 20:51:03 +0000

    Removed a pesky dot.

--- main.hs
+++ main.hs
@@ -1,7 +1,7 @@
 
 -- Arithmetic Series
 aseries :: Int -> Int -> Int -> [Int]
-aseries a d x = . zipWith (+) (replicate x a)([n*d | n <- [0..x]])
+asequence a d x = zipWith (+) (replicate x a)([n*d | n <- [0..x]])
 
 main :: IO ()
-main = print $ aseries 1 2 100
+main = print $ asequence 1 2 100
