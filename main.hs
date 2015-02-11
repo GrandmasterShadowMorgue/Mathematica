2bea7093f640e3f8b430dd4113471d4706e688f1
GrandmasterShadowMorgue <kavijay1997@yahoo.in>
2015-02-11 22:43:14 +0000

    Added collatz sequence and modified the arithmetic series section.

--- main.hs
+++ main.hs
@@ -1,7 +1,30 @@
+import Data.List
 
 -- Arithmetic Series
-aseries :: Int -> Int -> Int -> [Int]
-asequence a d x = zipWith (+) (replicate x a)([n*d | n <- [0..x]])
+asequence :: Int -> Int -> Int -> [Int]
+asequence a d x = [a+n*d | n <- [0..x]]
+
+aseries :: Int -> Int -> Int -> String
+aseries a d x = concat $ intersperse "+" $ map show $ asequence a d x
+
+tn :: Int -> Int -> Int -> Int
+tn n a d = a + (n-1)*d
+
+sn :: Int -> Int -> Int -> Int
+sn n a d = (n `div` 2) * (2*a + (n-1)*d)
+--------------------------------------------------
+
+-- Collatz Sequence
+next n
+	| even n = n `div` 2
+	| otherwise = 3*n + 1
+
+collatz :: Int -> [Int]
+collatz 1 = [1]
+collatz x = x : collatz (next x)
+--------------------------------------------------
 
 main :: IO ()
-main = print $ asequence 1 2 100
+main = do
+	putStrLn $ aseries (1) (2) (100)
+	putStrLn $ concat . intersperse "->" $ map show $ collatz 13
